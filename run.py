dc6722f93aa5184ab490166525dd7e9dc0bc5b00
Medone <aarabelmehdi4@gmail.com>
2020-03-09 02:05:18 +0100

    renew

--- run.py
+++
@@ -1,8 +0,0 @@
-
-import uuid 
-  
-# joins elements of getnode() after each 2 digits. 
-  
-print ("The MAC address in formatted way is : ") 
-print (':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff) 
-for ele in range(0,8*6,8)][::-1]))
