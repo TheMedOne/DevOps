08200b47798d4c4efd592a92c7513bdd77722115
Medone <aarabelmehdi4@gmail.com>
2020-03-11 23:11:23 +0100

    test restore

--- runMac@Display.py
+++ runMac@Display.py
@@ -3,6 +3,8 @@ import uuid
   
 # joins elements of getnode() after each 2 digits. 
   
+idit
+  
 print ("The MAC address in formatted way is : ") 
 print (':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff) 
 for ele in range(0,8*6,8)][::-1]))
